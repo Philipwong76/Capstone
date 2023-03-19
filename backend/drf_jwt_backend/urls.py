1dc5f17f9539884f78cdb3dcfd65e906f4977da3
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-19 18:04:39 -0400

    created Serializers.py + urls.py for soda_product and updated view.py

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
+    path('api/soda_product/', include('soda_product.urls')),
 ]
---
+++ backend/soda_product/Serializers.py
@@ -0,0 +1,8 @@
+
+from rest_framework import serializers
+from .models import Soda_product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Soda_product
+        fields = ['id', 'product', 'price', 'size', 'flavor']
