d8b4c7a8916cf4e059d7b4e704059a54a5881fa5
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-19 19:25:39 -0400

    Registered soda_product to admin site + updated view.py

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -78,6 +78,10 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
+
 WSGI_APPLICATION = 'drf_jwt_backend.wsgi.application'
 
 
--- backend/soda_product/views.py
+++ backend/soda_product/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .Serializers import ProductSerializer
+from .models import Soda_product
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def soda_product_list(request):
 
-    return Response('ok')
+    if request.method == 'GET':
+        sodas = Soda_product.objects.all()
+        serializer = ProductSerializer(sodas, many=True)
+        return Response(serializer.data)
+    
+
+    elif request.method == 'POST':
+            serializer = ProductSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
