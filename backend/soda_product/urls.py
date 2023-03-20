5599d7dc877045cb0d09857e298d9ae46aed69c5
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-19 20:08:10 -0400

    Completed backend.soda_product.views.py

--- backend/soda_product/urls.py
+++ backend/soda_product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.soda_product_list),
+    path('<int:pk>/',views.soda_detail),
 ]
--- backend/soda_product/views.py
+++ backend/soda_product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,3 +23,18 @@ def soda_product_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(['GET', 'PUT','DELETE'])
+def soda_detail(request,pk):
+    soda = get_object_or_404(Soda_product, pk=pk)
+    if request.method == 'GET':
+         serializer = ProductSerializer(soda)
+         return Response(serializer.data)
+    elif request.method == 'PUT':
+         soda = get_object_or_404(Soda_product, pk=pk)
+         serializer = ProductSerializer(soda, data=request.data)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         return Response(serializer.data)
+    elif request.method == 'DELETE':
+         soda.delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)
