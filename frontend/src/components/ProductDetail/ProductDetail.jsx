08a451307226eb7b718ab7c429b00a1d490233d1
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-31 15:57:45 -0400

    Modified ProductDeail to view id from soda.js.(unable to match params for the time being)

--- frontend/src/components/ProductDetail/ProductDetail.jsx
+++ frontend/src/components/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { sodas } from "../../pages/Product/sodas";
 
-function ProductDetail({match}) {
-    const product = sodas.filter((props) => props.id === match.id )
+const ProductDetail = (props) => {
+    const product = sodas.find((soda) => soda.id)
     return ( 
         <div>
             {product.product}
         </div>
-     )
+     );
 }
  
 export default ProductDetail;
--- frontend/src/components/ViewProductPage/ViewProductPage.jsx
+++ frontend/src/components/ViewProductPage/ViewProductPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import ProductDetail from "../ProductDetail/ProductDetail";
 
-const ViewProductPage = (props) => {
+const ViewProductPage = () => {
     return (
         <Container>
             <h1>Product</h1>
-            <ProductDetail id={props.id}/>
+            <ProductDetail/>
         </Container>
     );
 }
